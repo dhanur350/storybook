6c2222fef07f9af6cd3a960a8d18d00cb92035c4
dhanur350 <dhanur297@gmail.com>
2023-04-10 13:26:31 +0530

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css'
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(<App />);
